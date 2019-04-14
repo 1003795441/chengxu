a4f5dc5000b105e14664571ab3943515cbd50ecf
chengxu <1003795441@qq.com>
2019-04-14 21:25:47 +0800

    createCommon

---
+++ Hstay/pms/src/main/java/com/example/pms/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.example.pms.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+    @RestController
+    public class HelloController {
+    @RequestMapping(value = "/hello")
+    public String hello(){
+        return "hello";
+    }
+}
