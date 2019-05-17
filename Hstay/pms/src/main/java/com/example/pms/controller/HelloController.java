7fe6c698cde37fe01ad2d4367488f85cad1b670e
chengxu <chengxu@hhotel.com>
2019-05-17 13:48:28 +0800

    注入spring+Security

--- Hstay/pms/src/main/java/com/example/pms/controller/HelloController.java
+++ Hstay/pms/src/main/java/com/example/pms/controller/HelloController.java
@@ -1,12 +1,20 @@
 package com.example.pms.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
-    @RestController
-    public class HelloController {
-    @RequestMapping(value = "/hello")
+@Controller
+public class HelloController {
+    @RequestMapping(value = "/login/form")
     public String hello(){
         return "hello";
+    }
+
+
+    @RequestMapping(value = "/login/form1")
+    public String hello1(){
+        return "hello1";
     }
 }
